ca20dbac5af53682f7c394cef5f6ff6789af4e12
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-14 22:51:03 -0500

    Java and Python

---
+++ CS-04-Values_and_References/src/Homework_Coding_in_Java_and_Python/SumAnArray.java
@@ -0,0 +1,16 @@
+package Homework_Coding_in_Java_and_Python;
+
+public class SumAnArray {
+    public static void main(String[] args) {
+
+
+        int sum = 0;
+        int[] nums = {3, 4, 5, 6};
+
+        for (int n : nums) {
+            sum += n;
+        }
+        System.out.println("Sum of array: " + sum);
+
+    }
+}

e9ad5d40d13434d39f5a5a78ecac1c05ee8761f9
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-21 14:31:42 -0500

    Java and Python

---
+++ CS-04-Values_and_References/src/Homework_Coding_in_Java_and_Python/IsPalindrom.java
@@ -0,0 +1,32 @@
+package Homework_Coding_in_Java_and_Python;
+
+public class IsPalindrom {
+
+    public boolean checkIfStringIsPalindrom (String word){
+        boolean result =true;
+        int first = 0;
+        int last = word.length()-1;
+
+        while (first < last){
+            if (word.charAt(first) != word.charAt(last)){
+                result = false;
+                break;
+            }
+            first++;
+            last--;
+        }
+
+        return  result;
+        //The time complexity is On/2 = O(n);
+        //The space complexity is O(1)
+    }
+
+    public static void main(String[] args) {
+        String word= "level";
+        IsPalindrom test = new IsPalindrom();
+        boolean result = test.checkIfStringIsPalindrom(word);
+        System.out.println(result);
+
+    }
+
+}

d9a5560ef68a153f5a91030354572a6e78c30ae2
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-29 19:25:50 -0500

    Interpreters - Homework

--- CS-07-Interpreters/src/Calculator.java
+++ CS-07-Interpreters/src/Calculator.java
@@ -19,10 +19,10 @@ public class Calculator {
 
         try {
             String operator = expr[0];
-            int operand1 = Integer.parseInt(expr[1]);
-            int operand2 = Integer.parseInt(expr[2]);
+            double operand1 = Double.parseDouble(expr[1]);
+            double operand2 = Double.parseDouble(expr[2]);
 
-            int result = evaluate(operator, operand1, operand2);
+            double result = evaluate(operator, operand1, operand2);
             System.out.println(result);
         } catch (Exception e) {
            e.printStackTrace();
@@ -32,7 +32,7 @@ public class Calculator {
         scanner.close();
     }
 
-    private static int evaluate(String operator, int operand1, int operand2) {
+    private static double evaluate(String operator, double operand1, double operand2) {
         switch (operator) {
             case "ADD":
                 return operand1 + operand2;
@@ -45,6 +45,13 @@ public class Calculator {
                     throw new IllegalArgumentException("Division by zero is not supported");
                 }
                 return operand1 / operand2;
+            case "MOD":
+                if (operand2 == 0) {
+                    throw new IllegalArgumentException("Modulo by zero is not supported");
+                }
+                return operand1 % operand2;
+            case "POW":
+                return Math.pow(operand1, operand2);
             default:
                 throw new UnsupportedOperationException("Unsupported operation: " + operator);
         }

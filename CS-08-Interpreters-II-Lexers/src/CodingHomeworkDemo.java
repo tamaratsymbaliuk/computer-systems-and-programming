ed9fbf378baa76c4a1b61d3627e5e0396d720762
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-10 08:21:33 -0500

    HomeWork

---
+++ CS-08-Interpreters-II-Lexers/src/CodingHomeworkDemo.java
@@ -0,0 +1,23 @@
+public class CodingHomeworkDemo {
+    public static void main(String[] args) {
+
+        String input = """
+                config "num_users" = 100
+                config "num_requests" = 100
+                update "num_users" = 200
+                compute "result" = %num_users + %num_requests
+                loop "i" from 1 to 5
+                compute "iteration_result_%i" = %num_users * %i
+                end
+                if %num_users > 1000
+                update "status" = "high load"
+                else
+                update "status" = "normal load"
+                end
+                """;
+        Lexer lexer = new Lexer(input);
+        for (Lexer.Token token : lexer) {
+            System.out.println(token);
+        }
+    }
+}

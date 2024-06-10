b548822253e0ba9f2d9ed3bd4ea64db583ebd689
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-09 19:25:50 -0500

    InterpretersLexers

--- CS-08-Interpreters-II-Lexers/src/Demo.java
+++ CS-08-Interpreters-II-Lexers/src/Demo.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class Demo {
+    public static void main(String[] args) {
+
+        String input = """
+                config "num_users" = 100
+                config "num_requests" = 100
+                update "num_users" = 200
+                compute "result" = %num_users + %num_requests
+                """;
+        Lexer lexer = new Lexer(input);
+        for (Lexer.Token token: lexer) {
+            System.out.println(token);
+        }
+
+//        ArrayList<Lexer.Token> tokens = new ArrayList<>();
+//        tokens.add(new Lexer.Token(Lexer.TokenType.CONFIG, "Config"));
+//        tokens.add(new Lexer.Token(Lexer.TokenType.STRING, "num_users"));
+//        tokens.add(new Lexer.Token(Lexer.TokenType.ASSIGNMENT, "="));
+//        tokens.add(new Lexer.Token(Lexer.TokenType.NUMBER, "100"));
+//
+//        for (Lexer.Token token: tokens) {
+//            System.out.println(token);
+//        }
+    }
 
 }

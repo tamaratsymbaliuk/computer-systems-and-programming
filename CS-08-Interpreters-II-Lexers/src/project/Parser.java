d26eac31fb56135fe1237346154984e531b8a3bb
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-14 22:15:45 -0500

    Parser

--- CS-08-Interpreters-II-Lexers/src/project/Parser.java
+++ CS-08-Interpreters-II-Lexers/src/project/Parser.java
@@ -1,29 +1,30 @@
 package project;
 
+import java.util.List;
+
 public class Parser {
-
-    abstract class ASTNode {
-
-    }
-    class Number extends ASTNode {
-        private final int value;
-        Lexer.Token numberToken;
-
-        public Number(Lexer.Token numberToken) {
-            this.numberToken = numberToken;
-            this.value = Integer.parseInt(numberToken.value);
-        }
+    private final List<Lexer.Token> tokens;
+    private int currentPos;
+    private Lexer.Token currentToken;
+    Parser(List<Lexer.Token> tokens) {
+        this.tokens = tokens;
+        currentPos = 0;
+        currentToken = tokens.get(currentPos);
     }
 
-    class BinaryOp extends ASTNode {
-        ASTNode right;
-        ASTNode left;
-        Lexer.Token operationToken;
-
-        public BinaryOp(ASTNode right, ASTNode left, Lexer.Token operationToken) {
-            this.right = right;
-            this.left = left;
-            this.operationToken = operationToken;
-        }
+    public ASTNode parse() {
+        return term();
     }
+
+    private ASTNode term() {
+        ASTNode factor = factor();
+
+        while ()
+
+    }
+
+    private ASTNode factor() {
+        return null;
+    }
+
 }

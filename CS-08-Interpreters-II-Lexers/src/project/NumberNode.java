451478a1f3d49e5a8c7f3452af1a27ab3b43bd27
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-15 14:19:52 -0500

    NumberNode

--- CS-08-Interpreters-II-Lexers/src/project/NumberNode.java
+++ CS-08-Interpreters-II-Lexers/src/project/NumberNode.java
@@ -2,10 +2,15 @@ package project;
 
 public class NumberNode extends ASTNode {
     final int value;
-    Lexer.Token numberToken;
+    Token numberToken;
 
-    public NumberNode(Lexer.Token numberToken) {
+    public NumberNode(Token numberToken) {
         this.numberToken = numberToken;
         this.value = Integer.parseInt(numberToken.value);
+    }
+
+    @Override
+    public void print(String indent) {
+        System.out.println(indent + "N{" + value + '}');
     }
 }

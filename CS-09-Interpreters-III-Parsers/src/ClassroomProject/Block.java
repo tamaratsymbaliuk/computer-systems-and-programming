0ea1e860a2c78cd8d88a4184da777245e2201f06
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-23 12:51:17 -0500

    Block

---
+++ CS-09-Interpreters-III-Parsers/src/ClassroomProject/Block.java
@@ -0,0 +1,19 @@
+package ClassroomProject;
+
+import java.util.List;
+
+public class Block extends ASTNode {
+    List<ASTNode> statements;
+
+    Block(List<ASTNode> statements) {
+        this.statements = statements;
+    }
+    @Override
+    public void print(String indent) {
+        for (ASTNode node : statements) {
+            node.print("  ");
+        }
+    }
+
+
+}

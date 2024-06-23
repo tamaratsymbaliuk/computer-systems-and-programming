903b9a99bba9b6193964cf999f6cb2cf396b3b58
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-23 12:48:48 -0500

    Classroom project - Var Class

---
+++ CS-09-Interpreters-III-Parsers/src/ClassroomProject/Var.java
@@ -0,0 +1,20 @@
+package ClassroomProject;
+
+import java.sql.SQLOutput;
+
+public class Var extends  ASTNode {
+    Token token;
+    String name;
+
+    Var(Token token) {
+        this.token = token;
+        this.name = token.value;
+    }
+
+    @Override
+    public void print(String indent) {
+        System.out.println(indent + "Var{" + name + '}');
+    }
+
+
+}

8ef40ed90f6c3ee48dde79639e3aa7034ef2cfe6
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-23 13:25:38 -0500

    Classroom project - Var Class

--- CS-09-Interpreters-III-Parsers/src/ClassroomProject/Var.java
+++ CS-09-Interpreters-III-Parsers/src/ClassroomProject/Var.java
@@ -6,7 +6,7 @@ public class Var extends  ASTNode {
     Token token;
     String name;
 
-    Var(Token token) {
+   public Var(Token token) {
         this.token = token;
         this.name = token.value;
     }

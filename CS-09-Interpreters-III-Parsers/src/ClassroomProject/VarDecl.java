ce0c92b36b6b24ed21b992937b11f9e23521be54
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-23 13:25:49 -0500

    Classroom project - VarDecl Class

--- CS-09-Interpreters-III-Parsers/src/ClassroomProject/VarDecl.java
+++ CS-09-Interpreters-III-Parsers/src/ClassroomProject/VarDecl.java
@@ -6,7 +6,7 @@ public class VarDecl extends  ASTNode {
     Var varNode;
     ASTNode expr;
 
-    VarDecl(Var varNode, ASTNode expr) {
+   public  VarDecl(Var varNode, ASTNode expr) {
         this.varNode = varNode;
         this.expr = expr;
     }

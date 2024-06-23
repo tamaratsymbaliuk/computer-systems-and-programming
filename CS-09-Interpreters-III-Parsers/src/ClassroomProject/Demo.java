c8d9160144aa74728757819a427789b2f1432227
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-23 12:47:43 -0500

    Classroom project - Demo Class

--- CS-09-Interpreters-III-Parsers/src/ClassroomProject/Demo.java
+++ CS-09-Interpreters-III-Parsers/src/ClassroomProject/Demo.java
@@ -5,34 +5,36 @@ import java.util.List;
 public class Demo {
     public static void main(String[] args) {
 
-        String input = """
-                config "num_users" = 100
-                config "num_requests" = 100
-                update "num_users" = 200
-                compute "result" = %num_users + %num_requests
+       String input = """
+                3 + 5 * (10 - 1);
+                var x = 4 - 5;
+                x = 1
                 """;
         Lexer lexer = new Lexer(input);
         for (Token token: lexer) {
             System.out.println(token);
         }
-        System.out.println("Second example");
-        /*String input1 = """
+        List<Token> tokens = lexer.getTokens();
+        Parser parser = new Parser(tokens);
+        ASTNode root = parser.parse();
+        root.print("   ");
+
+        /*System.out.println("Second example");
+        String input1 = """
                 3 * 5 * 10 * 4
                 """;
         Lexer lexer1 = new Lexer(input1);
 
          */
-        List<Token> tokens = Lexer.getHardcodedTokens();
+      //  List<Token> tokens = Lexer.getHardcodedTokens();
         // List<Token> tokens = new ArrayList<Token>();
         /*for (Token token: lexer1) {
             //   tokens.add(token);
             System.out.println(token);
-        }*/
+        }
 
-        Parser parser = new Parser(tokens);
-        ASTNode root = parser.parse();
 
-        root.print("   ");
+         */
 
 
 //        ArrayList<project.Lexer.Token> tokens = new ArrayList<>();

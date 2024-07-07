922856c2b8250a0e4e4fa8debbbc8fdd47f70d34
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-07 18:22:06 -0500

    Homework Triangle class

--- CS-10-OOP-Basics/src/shapes/Triangle.java
+++ CS-10-OOP-Basics/src/shapes/Triangle.java
@@ -1,14 +1,22 @@
 package shapes;
 
 public class Triangle extends Shape {
+    private double sideA, sideB, sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
 
     @Override
     public double calculateArea() {
-        return 0;
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return sideA + sideB + sideC;
     }
 }

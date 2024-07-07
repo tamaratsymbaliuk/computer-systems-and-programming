6ae4be5da088674bdfe291ace4a3590557e26732
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-07 18:23:50 -0500

    Homework Square class

---
+++ CS-10-OOP-Basics/src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Shape {
+    private double side;
+    public Square( double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+}

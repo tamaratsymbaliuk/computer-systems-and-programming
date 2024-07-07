43465e1813dcfb5fcdf493b33046144f5439083d
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-07 18:17:09 -0500

    Homework Shape class

---
+++ CS-10-OOP-Basics/src/shapes/Shape.java
@@ -0,0 +1,6 @@
+package shapes;
+// Write a program to create a class that represents a shape. Include methods to calculate its area and perimeter. Implement subclasses for different shapes like circle, triangle, and square
+public abstract class Shape {
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+}

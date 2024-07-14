038d8002f55f8c8e50b6c13c2466b9754e5d77ec
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-14 13:01:25 -0500

    SavingsAccount class

---
+++ CS-11-OOP-Advanced/src/BankSystemHW/SavingsAccount.java
@@ -0,0 +1,9 @@
+package BankSystemHW;
+
+public class SavingsAccount extends Account {
+    private double interestRate;
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+}

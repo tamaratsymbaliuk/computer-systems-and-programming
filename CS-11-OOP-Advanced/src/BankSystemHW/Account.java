b739f077eb7920f1968903bde354c9c131f77f6a
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-14 13:45:13 -0500

    Account class

--- CS-11-OOP-Advanced/src/BankSystemHW/Account.java
+++ CS-11-OOP-Advanced/src/BankSystemHW/Account.java
@@ -10,4 +10,35 @@ public abstract class Account implements AccountOperations {
         this.balance = balance;
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
+
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited " + amount + ". New balance: " + balance);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+        }
+
+    @Override
+    public void withdraw(double amount) {
+        if(amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew " + amount + ". New balance: " + balance);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+
+        }
+
+    @Override
+    public String toString() {
+        return "Account(" + accountNumber + ", Balance: " + balance + ")";
+    }
 }
+

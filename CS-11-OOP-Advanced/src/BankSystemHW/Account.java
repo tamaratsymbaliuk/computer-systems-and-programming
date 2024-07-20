9d4cfe8dade0f7caa02dc955b34a01cdb55ab9ae
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-20 15:46:59 -0500

    Bank system HW Account

--- CS-11-OOP-Advanced/src/BankSystemHW/Account.java
+++ CS-11-OOP-Advanced/src/BankSystemHW/Account.java
@@ -1,19 +1,55 @@
 package BankSystemHW;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Account implements AccountOperations {
 
     private String accountNumber;
     private double balance;
+    private String pin;
+    private List<String> transactionHistory;
 
     public Account(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.transactionHistory = new ArrayList<>();
+    }
+
+    public Account(String accountNumber, double balance, String pin) {
+        this(accountNumber, balance);
+        this.pin = pin;
+    }
+    public Account(String accountNumber) {
+        this(accountNumber, 0.0);
     }
 
     public double getBalance() {
         return balance;
     }
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public List<String> getTransactionHistory() {
+        return transactionHistory;
+    }
+    public void addTransaction(String transaction) {
+        transactionHistory.add(transaction);
+    }
+
+    public boolean verifyPin(String pin) {
+        return this.pin != null && this.pin.equals(pin);
+    }
+    public void changePin(String oldPin, String newPin) {
+        if (verifyPin(oldPin)) {
+            this.pin = newPin;
+            System.out.println("PIN changed successfully.");
+        } else {
+            System.out.println("Incorrect old PIN.");
+        }
+        }
 
     @Override
     public void deposit(double amount) {
@@ -35,7 +71,6 @@ public abstract class Account implements AccountOperations {
         }
 
         }
-
     @Override
     public String toString() {
         return "Account(" + accountNumber + ", Balance: " + balance + ")";

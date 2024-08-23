d1b4f801893a8ac26b30232a27402b5b524f5828
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-08-22 21:49:55 -0500

    Lecture - Java extra: collections, enums, lambda

---
+++ CS-14-Java-Collections-Enums-Lambda/src/beforeCollectionsVectors/CollectionDemo.java
@@ -0,0 +1,17 @@
+package beforeCollectionsVectors;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+        int arr[] = new int[] {1,2,3,4};
+        Vector<Integer> v = new Vector<>();
+        Hashtable<Integer, String> h = new Hashtable<>();
+        v.addElement(1);
+        h.put(2, "4geeks");
+        System.out.println(arr[0]);
+        System.out.println(v.elementAt(0));
+        System.out.println(h.get(1));
+    }
+}

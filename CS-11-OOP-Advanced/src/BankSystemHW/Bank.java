7405ebf85bddfb1e884dfc8be72dabe27f90d8ff
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-08-26 15:10:25 -0600

    HW - adding Lambda, Collections, enums to the Bank System

--- CS-11-OOP-Advanced/src/BankSystemHW/Bank.java
+++ CS-11-OOP-Advanced/src/BankSystemHW/Bank.java
@@ -1,4 +1,12 @@
 package BankSystemHW;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private Map<String, Customer> customers;
+
+    public Bank() {
+        this.customers = new HashMap<>();
+    }
 }
